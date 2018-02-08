4d5f3c811c13ff36e709c2f5d4416fb2d4318789
Michael Lin <unadlib@gmail.com>
2018-02-09 03:47:17 +0800

    initial

---
+++ src/lib/module.js
@@ -0,0 +1,25 @@
+class Module {
+  constructor(...args) {
+    this._initialize();
+  }
+
+  _initialize() {
+
+  }
+
+  async _asyncInitialize() {
+
+  }
+
+  __moduleWillMount() {
+
+  }
+
+  __moduleDidMount() {
+
+  }
+
+  __moduleWillUnmount() {
+
+  }
+}
