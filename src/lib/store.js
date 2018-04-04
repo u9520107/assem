f83c418470437bf29375ee61e0656ebf7914d59f
Michael Lin <unadlib@gmail.com>
2018-04-05 00:11:37 +0800

    update

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,36 +1,7 @@
 import Subscriber from './subscriber';
 
-// const subscriber = new Subscriber();
-// const subscribe = subscriber.add.bind(subscriber);
-//
-// export default class Store {
-//   // constructor(initialState = {}, subscribe) {
-//   //   if (typeof subscribe !== 'function') {
-//   //     throw new Error(`default Store's subscribe must be a function.`);
-//   //   }
-//   //   this._state = initialState;
-//   //   this._subscribe = subscribe;
-//   // }
-//   //
-//   // getState() {
-//   //   return this._state;
-//   // }
-//   //
-//   // setState(state) {
-//   //   const [key, value] = Object.entries(state);
-//   //   this._state[key] = value;
-//   //   this._subscribe({
-//   //     key,
-//   //     value,
-//   //   });
-//   // }
-// }
-//
-// export function createStore(reducers) {
-//   return Store
-// }
+const INIT_STORE = Symbol ? Symbol() : '@@@init@@@';
 
-const initStore = Symbol ? Symbol() : '@@@init@@@';
 const subscriber = new Subscriber();
 const subscribe = subscriber.add.bind(subscriber);
 subscribe.report = subscriber.report.bind(subscriber);
@@ -38,7 +9,7 @@ subscribe.report = subscriber.report.bind(subscriber);
 const store = {
   getState: function () {
     if (typeof this._state === 'undefined') {
-      this._dispatch({ type: initStore });
+      this._dispatch({ type: INIT_STORE });
     }
     return this._state;
   },
@@ -47,12 +18,12 @@ const store = {
       typeof action !== 'object' ||
       (
         typeof action.type !== 'string' &&
-        action.type !== initStore
+        action.type !== INIT_STORE
       )
     ) {
       throw new Error(`invalid action`);
     }
-    if (action.type === initStore) {
+    if (action.type === INIT_STORE) {
       this._state = {};
     }
     Object.entries(this._reducers).forEach(([key, reducer]) => {
