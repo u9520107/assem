b3d54954e0eff1e075f4c995936ebe51ccbcc575
Michael Lin <unadlib@gmail.com>
2018-04-05 00:12:48 +0800

    update

--- src/core/actionTypes.js
+++ src/core/actionTypes.js
@@ -1,12 +1,5 @@
 import Enum from '../lib/Enum';
 
-// export default new Enum([
-//   'init',
-//   'initSuccess',
-//   'reset',
-//   'resetSuccess',
-// ]);
-
 export default function getActionTypes(
   actionsTypes = [],
   prefix
